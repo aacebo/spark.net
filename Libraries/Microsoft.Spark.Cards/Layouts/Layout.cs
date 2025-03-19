15e712d8def41934b5454262d59a3690bbabf8d5
Alex Acebo <aacebowork@gmail.com>
2025-03-19 16:42:57 -0700

    add layouts

---
+++ Libraries/Microsoft.Spark.Cards/Layouts/Layout.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace Microsoft.Spark.Cards;
+
+public abstract class Layout(CardType type)
+{
+    /// <summary>
+    /// the layout card type
+    /// </summary>
+    [JsonPropertyName("type")]
+    [JsonPropertyOrder(0)]
+    public CardType Type { get; set; } = type;
+
+    /// <summary>
+    /// Controls for which card width the layout should be used.
+    /// </summary>
+    [JsonPropertyName("targetWidth")]
+    [JsonPropertyOrder(1)]
+    public TargetWidth? TargetWidth { get; set; }
+
+    public Layout WithTargetWidth(TargetWidth value)
+    {
+        TargetWidth = value;
+        return this;
+    }
+}
