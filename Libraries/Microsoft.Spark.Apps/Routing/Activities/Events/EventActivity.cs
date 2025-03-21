11a4fed5089764773ee4bb2243effa3424cc5d20
aacebo <aacebowork@gmail.com>
2025-03-21 00:41:41 -0400

    add invoke and event

---
+++ Libraries/Microsoft.Spark.Apps/Routing/Activities/Events/EventActivity.cs
@@ -0,0 +1,35 @@
+using Microsoft.Spark.Api.Activities;
+
+namespace Microsoft.Spark.Apps.Routing;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class EventAttribute() : ActivityAttribute<EventActivity>(ActivityType.Event.Value)
+{
+}
+
+public partial interface IRoutingModule
+{
+    public IRoutingModule OnEvent(Func<IContext<EventActivity>, Task> handler);
+}
+
+public partial class RoutingModule : IRoutingModule
+{
+    public IRoutingModule OnEvent(Func<IContext<EventActivity>, Task> handler)
+    {
+        Router.Register(new Route()
+        {
+            Handler = context => handler(context.ToActivityType<EventActivity>()),
+            Select = activity =>
+            {
+                if (activity is EventActivity @event)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        return this;
+    }
+}
---
+++ Libraries/Microsoft.Spark.Apps/Routing/Activities/Invokes/InvokeActivity.cs
@@ -0,0 +1,35 @@
+using Microsoft.Spark.Api.Activities;
+
+namespace Microsoft.Spark.Apps.Routing;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class InvokeAttribute() : ActivityAttribute<InvokeActivity>(ActivityType.Invoke.Value)
+{
+}
+
+public partial interface IRoutingModule
+{
+    public IRoutingModule OnInvoke(Func<IContext<InvokeActivity>, Task> handler);
+}
+
+public partial class RoutingModule : IRoutingModule
+{
+    public IRoutingModule OnInvoke(Func<IContext<InvokeActivity>, Task> handler)
+    {
+        Router.Register(new Route()
+        {
+            Handler = context => handler(context.ToActivityType<InvokeActivity>()),
+            Select = activity =>
+            {
+                if (activity is InvokeActivity invoke)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        return this;
+    }
+}
