c163b28eaa0a571e3abd3403802d24a3a3d81bf1
aacebo <aacebowork@gmail.com>
2025-04-09 20:42:46 -0400

    Create DevToolsPlugin.cs

---
+++ Libraries/Microsoft.Spark.Plugins.DevTools/DevToolsPlugin.cs
@@ -0,0 +1,9 @@
+using Microsoft.Spark.Apps.Plugins;
+
+namespace Microsoft.Spark.Plugins.DevTools;
+
+[Plugin(name: "Microsoft.Spark.Plugins.DevTools", version: "0.0.0")]
+public class DevToolsPlugin
+{
+
+}
